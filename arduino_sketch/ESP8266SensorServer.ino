5699fdac5e3412372cf3e91e01612bb8c8a4ee87
saulsebrook <36156993+saulsebrook@users.noreply.github.com>
2026-02-11 05:15:11 +1000

    Update ESP8266SensorServer.ino

--- arduino_sketch/ESP8266SensorServer.ino
+++ arduino_sketch/ESP8266SensorServer.ino
@@ -91,16 +91,6 @@ void setup() {
   Serial.println(WiFi.localIP());
  
   unsigned status = bme.begin(0x77);
-  if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-        Serial.print("SensorID was: 0x"); 
-        Serial.println(bme.sensorID(),16);
-        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-        Serial.print("        ID of 0x60 represents a BME 280.\n");
-        Serial.print("        ID of 0x61 represents a BME 680.\n");
-        while (1) delay(10);
-    }
   bme.setSampling(Adafruit_BME280::MODE_FORCED,
                     Adafruit_BME280::SAMPLING_X1, // temperature
                     Adafruit_BME280::SAMPLING_X1, // pressure
